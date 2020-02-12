fd19b27612918ae6ec0532275cf4252079637ecc
elisagc <elisagcubero@gmail.com>
2020-02-12 21:13:26 +0100

    xml ok

---
+++ Tema16_XML/Ejercicio_1_libros_addChild/controlXML.php
@@ -0,0 +1,64 @@
+<?php
+include("./class/Conexion.php");
+
+
+if (isset($_POST["submit"])) {
+
+    $con = Conexion::getinstance("./config/config.json");
+    $query = "select * from book";
+
+    //tengo mi array de libros
+    $rows = $con->selectQuery($query);
+
+    //pasar array a SimpleXML, a este objeto ya se le puede añadir hijos
+    $exists = file_exists('mixml.xml');
+
+    //si no existe crear el xml a partir de las filas devueltas
+    // si existe, leer el xml y escribir sobre él
+    if (!$exists) {
+        $xml = arrayToXml($rows);
+        addChild($xml);
+    } else {
+        $xml = simplexml_load_file("mixml.xml");
+        addChild($xml);
+    }
+
+    $document = simplexml_load_file('mixml.xml');
+    echo "<h1>Documento guardado en mixml.xml</h1>";
+    var_dump($document);
+}
+
+
+
+function addChild($xml)
+{
+    $book = $xml->addChild("libro");
+    $book->addChild("id", $_POST['id']);
+    $book->addChild("isbn", $_POST['isbn']);
+    $book->addChild("title", $_POST['title']);
+    $book->addChild("author", $_POST['author']);
+    $book->addChild("stock", $_POST['stock']);
+    $book->addChild("price", $_POST['price']);
+    $xml->asXML('mixml.xml');
+}
+
+
+function arrayToXml($array, $rootElement = null, $xml = null)
+{
+    $_xml = $xml;
+    if ($_xml === null) {
+        $_xml = new SimpleXMLElement($rootElement !== null ? $rootElement : '<root/>');
+    }
+    foreach ($array as $k => $v) {
+        //cuando viene de la BBDD transformar el índice por la etiqueta
+        if (is_numeric($k)) {
+            $k = "libro";
+        }
+        if (is_array($v)) {
+            arrayToXml($v, $k, $_xml->addChild($k));
+        } else {
+            $_xml->addChild($k, $v);
+        }
+    }
+    return $_xml;
+}
--- Tema16_XML/Ejercicio_1_libros_addChild/index.php
+++ Tema16_XML/Ejercicio_1_libros_addChild/index.php
@@ -1,5 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-//Vamos a trabajar con un xml como si se tratase de una base de datos. 
-//Para ello vamos a utilizar el interface de inserción de libros y vamos a ir creando mas hijos en el árbol del xml
-//utilizando para ello el método addchild del Simplexml
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    El siguiente ejercicio:
+    <br>
+    1. Lee los libros de la BBDD
+    <br>
+    2. Transforma el array obtenido de la BBDD en un XML
+    <br>
+    3. Añade los datos del formulario en el XML generado
+    <br>
+    4. Comprueba si existe el fichero xml:
+    <br>
+    -si no existe, se crea a partir de la consulta a la BBDD
+    <br>
+    - si existe se añade el dato en el fichero creado anteriormente
+
+
+    <form action='controlXML.php' method='post'>
+        id<input type="text" name="id"><br>
+        isbn <input type="text" name="isbn"><br>
+        título<input type="text" name="title"><br>
+        author<input type="text" name="author"><br>
+        stock<input type="text" name="stock"><br>
+        precio<input type="text" name="price"><br>
+
+        <input type="submit" name="submit">
+    </form>
+</body>
+
+</html>
